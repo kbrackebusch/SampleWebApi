da02067ba9548fc41a584e158696b2c1d37aaa8d
Kirk Brackebusch <KirkB@wideorbit.com>
2018-12-06 21:00:01 -0800

    Initial

---
+++ SampleWebApi/Cache.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebApi
+{
+    public class Cache
+    {
+        private ConcurrentDictionary<(string a, string b), LinkedList> cache = new ConcurrentDictionary<(string a, string b), LinkedList>();
+
+        public bool TryGetValue(string listA, string listB, out LinkedList mergedList)
+        {
+            return cache.TryGetValue((listA, listB), out mergedList);
+        }
+
+        public void CacheQuery(string listA, string listB, LinkedList mergedList)
+        {
+            // ok for any thread to succed here.  Since both should have the same value
+            cache[(listA, listB)] = mergedList;
+        }
+    }
+}
---
+++ SampleWebApi/LinkedList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebApi
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node NextNode { get; set; }
+    }
+
+    public class LinkedList //: CustomCreationConverter
+    {
+        public Node Head { get; set; }
+        public Node LastNode { get; private set; }
+
+        public LinkedList()
+        {
+        }
+
+        public LinkedList(string spaceSeparatedString)
+        {
+            var array = spaceSeparatedString.Split(' ');
+        }
+
+        public void Add(Node node)
+        {
+            if(Head==null)
+            {
+                Head = node;
+                LastNode = Head;
+            }
+            else
+            {
+                LastNode.NextNode = node;
+                LastNode = node;
+            }
+        }
+
+    }
+
+    
+}
